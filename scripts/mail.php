eb5ecf9a914e5257076662f860bdf2bcb2b0650c
dep999 <149096642+dep999@users.noreply.github.com>
2023-12-29 20:20:22 +0200

    Add files via upload

--- scripts/mail.php
+++ scripts/mail.php
@@ -4,31 +4,35 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+function sanitize_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
-    $company = $_POST["company"];
-    $text = $_POST["text"];
-    $to = "tepposoininen1@gmail.com";
-
-
-    $subject = "Viesti henkilöltä $name";
-    $message = "Yritys: $company\nNumero: $phone\n\n$text";
-
+    // Sanitize user input
+    $name = sanitize_input($_POST["name"]);
+    $email = sanitize_input($_POST["email"]);
+    $phone = sanitize_input($_POST["phone"]);
+    $company = sanitize_input($_POST["company"]);
+    $text = sanitize_input($_POST["text"]);
+    $to = "teppos1234@gmail.com";
 
     // Validate user input
     if (empty($name) || empty($email) || empty($phone) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        
         header("Location: ../yhteydenotto.html?success=0");
         exit();
     }
 
-    
+    // Define subject and message
+    $subject = "Viesti henkilöltä $name";
+    $message = "Yritys: $company\nNumero: $phone\n\n$text";
 
     // Additional headers to prevent email injection
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
+    $headers = "From: " . filter_var($email, FILTER_SANITIZE_EMAIL) . "\r\n";
+    $headers .= "Reply-To: " . filter_var($email, FILTER_SANITIZE_EMAIL) . "\r\n";
     $headers .= "X-Mailer: PHP/" . phpversion();
 
     // Attempt to send the email
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,14 +1,21 @@
-
 function displayAlert() {
     const urlParams = new URLSearchParams(window.location.search);
     const successParam = urlParams.get('success');
 
+    // Set a delay of, for example, 1000 milliseconds (1 second)
+    const delay = 1000;
+
     if (successParam === '1') {
-        alert('Viesti lähetetty onnistuneesti');
+        // Show the success alert after the delay
+        setTimeout(function() {
+            alert('Viesti lähetetty onnistuneesti');
+        }, delay);
     } else if (successParam === '0') {
-        alert('Viestin lähetys epäonnistui');
+        // Show the failure alert after the delay
+        setTimeout(function() {
+            alert('Viestin lähetys epäonnistui');
+        }, delay);
     }
 }
-
 
 window.onload = displayAlert;
